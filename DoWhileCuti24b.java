45074df9b8401d1d52f31441b18374c5b61affa5
rockhubzz <compatiblewindows7@gmail.com>
2023-10-16 16:16:40 +0700

    Modifikasi Handle Kelebihan Request Cuti Percobaan 3

---
+++ DoWhileCuti24b.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DoWhileCuti24b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti (hari): ");
+        jatahCuti= sc.nextInt();
+
+        do{
+            System.out.print("Apakah anda ingin mengambil cuti? (y/t): ");
+            konfirmasi= sc.next();
+
+            if(konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("Jumlah cuti yang ingin diambil (hari): ");
+                jumlahHari= sc.nextInt();
+
+                if(jumlahHari <= jatahCuti){
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti anda: " +jatahCuti+ " hari.");
+                } else{
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi.");
+                    continue;
+                }
+            }
+        } while(jatahCuti > 0);
+    }
+}
